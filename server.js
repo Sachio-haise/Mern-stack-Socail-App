18113e0a4b9915720fd3840cfb23b418c5ab6f32
Sachio-haise <aungkaungmyatkpg777@gmail.com>
2022-07-16 21:58:30 +0630

    fix

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ const app = express();
 app.use(cors());
 
 app.use(
-  "/api",
   createProxyMiddleware({
     target: "https://mern-social-app-frontend2022.herokuapp.com/", //original url
     changeOrigin: true,
