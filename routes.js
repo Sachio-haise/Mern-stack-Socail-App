bd28af5e0c3425562a915c814d8b984490d13863
Sachio-haise <aungkaungmyatkpg777@gmail.com>
2022-07-16 22:24:08 +0630

    fix some server errors again 3

--- routes.js
+++ routes.js
@@ -33,8 +33,19 @@ const upload = multer();
 const Router = express.Router();
 
 Router.all(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://mern-social-app-frontend2022.herokuapp.com/"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
   next();
 });
 
