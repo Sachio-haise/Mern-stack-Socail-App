6b8d6b559b1b0683f071c2eb3406c24ce6c49705
Sachio-haise <aungkaungmyatkpg777@gmail.com>
2022-07-16 20:59:41 +0630

    fix error again

--- src/setUpProxy.js
+++ src/setUpProxy.js
@@ -2,7 +2,7 @@ const proxy = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    proxy("/", {
+    proxy({
       target: "https://mern-social-app-2022.herokuapp.com",
       // secure: false,
       changeOrigin: true,
