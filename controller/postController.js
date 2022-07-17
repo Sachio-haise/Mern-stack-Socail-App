3e5c7fb6db902c7efa53e28fd5a9f020b620d725
Sachio-haise <aungkaungmyatkpg777@gmail.com>
2022-07-17 18:50:06 +0630

    code server fix 1

--- controller/postController.js
+++ controller/postController.js
@@ -1,6 +1,6 @@
 import { Post } from "../Schema/postSchema.js";
 import { User } from "../Schema/userSchema.js";
-
+import fs from "fs";
 export const getPosts = async (req, res) => {
   var posts = await Post.find({})
     .populate("user")
@@ -18,32 +18,36 @@ export const getPosts = async (req, res) => {
 
 export const createPost = async (req, res) => {
   const { text, user_id } = req.body;
+  const { file } = req.files;
+  var post = await Post.create({
+    user: user_id,
+    text: text,
+    created_At: new Date().toISOString(),
+  });
+  if (file) {
+    const folderName = "post_" + post._id;
+    const fileName = req.body.text.substring(0, 5) + "-" + file.name;
+    const dir = "../client/public/Posts/" + folderName + "/";
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
+    }
 
-  var post;
-  if (req.file) {
-    const folderName = "post_" + req.post_id;
-    post = await Post.findByIdAndUpdate(
-      req.post_id,
+    file.mv(`${dir}${fileName}`, (err) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send(err);
+        return;
+      }
+    });
+    await Post.findByIdAndUpdate(
+      post._id,
       {
-        file:
-          "Posts/" +
-          folderName +
-          "/" +
-          req.file.fieldname +
-          "-" +
-          req.body.text.substring(0, 5) +
-          "_" +
-          req.file.originalname,
+        file: "Posts/" + folderName + "/" + fileName,
       },
       { new: true }
     );
-  } else {
-    post = await Post.create({
-      user: user_id,
-      text: text,
-      created_At: new Date().toISOString(),
-    });
   }
+
   await User.findByIdAndUpdate(
     user_id,
     {
