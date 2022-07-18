8b36db87d81f471dde0c57a29d0921de6e141d8a
Sachio-haise <aungkaungmyatkpg777@gmail.com>
2022-07-18 17:48:23 +0630

    fix

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const server_url = "http://localhost:5000";
+export const server_url = "https://mern-social-app-2022.herokuapp.com";
 //https://mern-social-app-2022.herokuapp.com
 export const Access_Control_Allow_Origin = "*";
